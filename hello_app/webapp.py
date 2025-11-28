52086c9c5b70ffed1cd488e2b223f2ce82de5c8c
Louise Deeth <127743702+LouiseDeeth@users.noreply.github.com>
2025-11-28 09:51:32 +0000

    Add hardcoded secret for security testing

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -4,3 +4,6 @@ from . import views  # noqa: F401
 
 # Time-saver: output a URL to the VS Code terminal
 # print('http://127.0.0.1:5000/hello/VSCode')
+
+# In hello_app/webapp.py
+AWS_SECRET_KEY = "AKIA1234567890"
