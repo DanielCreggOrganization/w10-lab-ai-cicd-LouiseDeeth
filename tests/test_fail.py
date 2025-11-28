29f3d8040872754cce488880cb73ef251f69d5f2
Louise Deeth <127743702+LouiseDeeth@users.noreply.github.com>
2025-11-28 09:36:20 +0000

    Fixed test

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -2,7 +2,7 @@ import pytest
 from hello_app.webapp import app
 
 def test_fail():
-    """This test is designed to fail."""
+    """This test now passes."""
     with app.test_client() as client:
         response = client.get('/')
-        assert response.status_code == 404  # Should fail, home route returns 200
+        assert response.status_code == 200  # Home route returns 200
